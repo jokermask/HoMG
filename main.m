e8a043da838a49cdbc15e521956acc14fb734778
jokermask <15521324430@163.com>
2017-05-12 16:28:34 +0800

    load images

--- main.m
+++ main.m
@@ -1,6 +1,21 @@
-path = './mall_dataset/frames'; 
-numofImages = 200 ;
+path = './mall_dataset/frames/'; 
+numofImages = 20 ;
+image_cells = cell(numofImages,1) ;
+E = cell(numofImages,1) ;
 for i=1:numofImages
-    I=imread([str,num2str(i),'.jpg']); %依次读取每一幅图像
-    %todo 存到cell 灰度转化 strip？
+    temp = i ;
+    prefix = 'seq_00' ;
+    while temp < 1000
+        prefix = strcat(prefix,'0') ;
+        temp = temp*10 ;
+    end
+    prefix = strcat(prefix,num2str(i)) ;
+    I=imread([path,prefix,'.jpg']); %依次读取每一幅图像
+    I=rgb2gray(I);
+    [Gmag,Gdir] = imgradient(I) ;
+    E{i}.Gmag = Gmag ;
+    E{i}.Gdir = Gdir ;
+    image_cells{i} = I ;
+    %todo strip高=wp/W？ 不一定好
+    
 end
